be8369b83a417f98425e7cb9e41d05c49d249973
Tim Grupczynski <t.grupczynski@googlemail.com>
2020-09-22 23:10:46 +0200

    Added Stat Class

---
+++ Assets/Scripts/CharacterStats.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterStats : MonoBehaviour
+{
+
+    [SerializeField] public int maxHealth = 100;
+    public int currentHealth { get; private set; }
+
+    public Stat armor;
+    public Stat damage;
+
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void Heal(int heal)
+    {
+        if (heal >= 0)
+        {
+            currentHealth += heal;
+            currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        damage = Mathf.Clamp(damage, 0, int.MaxValue);
+        currentHealth -= damage;
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        Debug.Log(transform.name + " died.");
+    }
+}
