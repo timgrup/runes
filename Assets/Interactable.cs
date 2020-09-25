f9d581de0feb4a6fcd133665060f962b423909cf
Tim Grupczynski <t.grupczynski@googlemail.com>
2020-09-25 11:30:54 +0200

    Added Player Interaction

--- Assets/Interactable.cs
+++ Assets/Interactable.cs
@@ -4,17 +4,36 @@ using UnityEngine;
 
 public class Interactable : MonoBehaviour
 {
-    float radius = 3.0f;
-    Transform target;
+    [SerializeField] float radius = 3.0f;
+    public Transform target;
+
+    bool hasInteracted;
+    bool interact;
+    bool isFocus;
+
+    private void Update()
+    {
+        if(interact && !hasInteracted && isFocus && target != null && CanInteract(target))
+        {
+            Interact();
+            hasInteracted = true;
+            interact = false;
+        }
+    }
 
     //Meant to be overwritten
     public virtual void Interact()
     {
-        Debug.Log("Interacting with" + target.name);
+        Debug.Log("Interacting..");
+    }
+
+    public void StartInteraction()
+    {
+        interact = true;
     }
 
     //Returns true, if target interacting with is in radius
-    public bool isReachable()
+    public bool isReachable(Transform target)
     {
         float distance = Vector3.Distance(target.position, transform.position);
         if(distance <= radius)
@@ -27,9 +46,25 @@ public class Interactable : MonoBehaviour
     }
 
     //Returns true, if target may interact with this interactable
-    public virtual bool CanInteract()
+    public virtual bool CanInteract(Transform target)
     {
-        return isReachable();
+        return isReachable(target);
+    }
+
+    // Called when the object starts being focused
+    public void OnFocused(Transform target)
+    {
+        isFocus = true;
+        this.target = target;
+        hasInteracted = false;
+    }
+
+    // Called when the object is no longer focused
+    public void OnDefocused()
+    {
+        isFocus = false;
+        target = null;
+        hasInteracted = false;
     }
 
     void OnDrawGizmosSelected()
